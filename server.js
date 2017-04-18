d0d99a5ae963a86a0e7633c3339b4d5ffc05912a
Baran Duzce <bararan@gmail.com>
2017-04-18 01:20:17 +0000

    App is now fully functional as a timestamp microservice.

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + '/views'));
+
 app.get("/", function(req, res) {
-    res.end("Maraba Televoli");
+    // res.end("Welcome to time server service!");
+    res.render();
+});
+
+app.get("/:ts", function(req, res) {
+    var date = new Date(req.params.ts);
+    if (!date.getDate()) date = new Date(parseInt(req.params.ts) * 1000); //Multiply by 1000 as JS uses microseconds since epoch!
+    var obj = {
+        unix: date.getTime() / 1000||null, //Divide by 1000 for the same reason.
+        natural: date.getTime() ? date.toDateString(): null
+    };
+    res.writeHead(200, {"type": "application/json"});
+    res.end(JSON.stringify(obj));
 });
 
 app.listen(8080);
